882f73dc3aa25778cb11b76dccf6bd680ad8f97a
janakhpon <jnovaxer@gmail.com>
2019-06-11 15:39:41 +0630

    GDEV003

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
 $(".dropdown-button").dropdown({
   inDuration: 300,
   outDuration: 225,
-  hover: true, // Activate on hover
-  belowOrigin: true, // Displays dropdown below the button
-  alignment: "right" // Displays dropdown with edge aligned to the left of button
+  hover: true,
+  belowOrigin: true,
+  alignment: "right" 
 });
 $(".dropdown-button1").dropdown({
     hover: false, 
-    belowOrigin: true, // Displays dropdown below the button
-    alignment: "right" // Displays dropdown with edge aligned to the left of button
+    belowOrigin: true, 
+    alignment: "right" 
 });
 $(".button-collapse").sideNav(); 
 
